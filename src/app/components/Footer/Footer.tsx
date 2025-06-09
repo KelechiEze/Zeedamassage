68c996b7472a22c37190c4248fb9354ee24df5e9
KelechiEze <kelechieze400@gmail.com>
2025-06-09 13:19:53 +0100

    yes

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -13,7 +13,7 @@ const Footer = () => {
 
   const socialLinks = [
     { icon: Facebook, href: '#', label: 'Facebook' },
-    { icon: Instagram, href: '#', label: 'Instagram' },
+    { icon: Instagram, href: 'https://www.instagram.com/zeedaa_coco/', label: 'Instagram' },
     { icon: X, href: '#', label: 'Twitter' }
   ];
 
